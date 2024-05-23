a04c12779edcccbb6015b304426fe5eb08698ae2
Yuukicoder <binhandoan9@gmail.com>
2024-05-23 15:42:34 +0700

    update Tracking

---
+++ GasStore/GasStore/src/java/DTO/Constants.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
+
+/**
+ *
+ * @author Admin
+ */
+public class Constants {
+    public static String GOOGLE_CLIENT_ID = "1038400586522-1g779ncgmua1fqbnblc92t35mjn7nf9c.apps.googleusercontent.com";
+
+	public static String GOOGLE_CLIENT_SECRET = "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";
+
+	public static String GOOGLE_REDIRECT_URI = "http://localhost:8888/GasStore2/login";
+
+	public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+
+	public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+
+	public static String GOOGLE_GRANT_TYPE = "authorization_code";
+}
